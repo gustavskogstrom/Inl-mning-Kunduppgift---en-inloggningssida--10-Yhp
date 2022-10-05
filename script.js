e1578fbb4b0b5af786184b78993962d6fc04b2bb
Gustav Skogström <gustav.skogstrom@gmail.com>
2022-10-05 11:49:16 +0200

    200

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*Här så hämtar jag id och classer*/
+
 const knapp = document.getElementById("knapp"); 
 const mainContent = document.querySelector(".form-content");
 const secondContent = document.querySelector(".second_content");
@@ -6,7 +6,6 @@ const checkLogin = localStorage.getItem("check");
 const error = document.getElementById("error");
 const loggaut = document.getElementById("loggaut");
 
-/*Här skapade jafg validate funktionen*/
 knapp.addEventListener("click", function () {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
@@ -19,7 +18,6 @@ knapp.addEventListener("click", function () {
         error.innerHTML = "Ditt namn eller lösennord är fel!";
     }
 });
-/*Denna funktionen ser om man är inloggad*/
 function kolla(){
     if (checkLogin) {
         mainContent.classList.add("loggedin");
@@ -27,7 +25,7 @@ function kolla(){
     }
     
 };
-/*Här har jag en funktion som loggar ut användaren*/
+
 kolla();
 loggaut.addEventListener("click", function(){
     localStorage.clear();
